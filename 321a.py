c051ad02bbbf3bbf946d60ad1293a286de389f41
AlexeiChizh <104721721+AlexeiChizh@users.noreply.github.com>
2022-05-30 08:54:56 +0300

    Update 321a.py
    Исправлено

--- 321a.py
+++ 321a.py
@@ -1,7 +1,8 @@
 l = [1, 4, 1 ,6, 'hello', 'a', 5, 'hello']
 l_u = []
-for i in l:
-  if i  not in l_u:
-    l_u.append(i)
+for i in range(0, len(l)):
+  for j in range(i + 1, len(l)):
+    if (l[i] == l[j]):
+      print(l[j])
+      l_u.append(l[j])
 print(l_u)
-# print(list(set(l)))
